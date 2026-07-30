cefaa8f9114c71b40c97e9ffab64a3809dea53e3
pradhyuman01 <pradhyuman019@gmail.com>
2026-07-30 15:01:19 +0530

    updated code

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -22,7 +22,7 @@ export default function Experience() {
                     </h3>
 
                     <p className="text-sm text-gray-400 mt-1">
-                        <span className="italic font-semibold"> Techlancea, Bhilwara </span> <br /> Internship • March 2026 - Present
+                        <span className="italic font-semibold"> Techlancea, Bhilwara </span> <br /> Internship • January 2026 - Present
                     </p>
 
                     <div className="text-gray-400 text-xs sm:text-base mt-4 space-y-2">
@@ -32,7 +32,7 @@ export default function Experience() {
                         </p>
                         <p className="flex items-baseline sm:items-center">
                             <span className="ri-arrow-right-s-fill text-base sm:text-xl font-bold text-purple-600"></span>
-                            Worked on 2 client-based frontend projects, implementing reusable UI components and responsive layouts.
+                            Worked on 2 client-based projects, integrating backend REST APIs and responsive layouts.
                         </p>
                         <p className="flex items-baseline sm:items-center">
                             <span className="ri-arrow-right-s-fill text-base sm:text-xl font-bold text-purple-600"></span>
